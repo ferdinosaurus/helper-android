ec07d8371a95b6787df9405a24bd5e750c8d0b36
danielnuhdaniel <danielnuh@malline.id>
2020-07-06 13:43:46 +0700

    Update CalendarActivity.java

--- CalendarActivity.java
+++ CalendarActivity.java
@@ -2,6 +2,7 @@ package com.e.kumpulanmateri.calender;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.Toast;
@@ -10,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.e.kumpulanmateri.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -36,8 +39,10 @@ public class CalendarActivity extends AppCompatActivity {
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                         Calendar newDate = Calendar.getInstance();
                         newDate.set(year, monthOfYear, dayOfMonth);
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-                        Toast.makeText(CalendarActivity.this, check(newDate) + "", Toast.LENGTH_SHORT).show();
+
+                        Toast.makeText(CalendarActivity.this, check(sdf.format(newDate.getTime()))+"", Toast.LENGTH_SHORT).show();
                     }
 
                 }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
@@ -47,7 +52,16 @@ public class CalendarActivity extends AppCompatActivity {
 
     }
 
-    boolean check(Calendar calendarSelect) {
+    boolean check(String strDate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date dateSelect = null;
+        try {
+            dateSelect = sdf.parse(strDate);
+        } catch (ParseException e) {
+            return false;
+        }
+        Calendar calendarSelect = Calendar.getInstance();
+        calendarSelect.setTime(dateSelect);
 
         Date dateNow = new Date(System.currentTimeMillis());
         Calendar calendarThis = Calendar.getInstance();
@@ -56,10 +70,16 @@ public class CalendarActivity extends AppCompatActivity {
         int selectWeek = calendarSelect.get(Calendar.WEEK_OF_YEAR);
         int thisWeek = calendarThis.get(Calendar.WEEK_OF_YEAR);
 
+        int selectDay = calendarSelect.get(Calendar.DAY_OF_YEAR);
+        int thisDay = calendarThis.get(Calendar.DAY_OF_YEAR);
+
         if (selectWeek == thisWeek){
-            if (dateNow.compareTo(calendarSelect.getTime())>0){
-                return true;
+            if (dateNow.compareTo(dateSelect)>0){
+                if (selectDay == thisDay){
+                    return false;
+                }else return true;
             }
+
         }
         return false;
     }
